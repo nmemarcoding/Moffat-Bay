1d156a4615325c286559b1dbadb47bee3ce7a115
nima memarzadeh <nmemarcoding@gmail.com>
2025-08-28 09:15:09 -0700

    Add SQL script to create initial database and tables for moffat_lodge

---
+++ create_tables.sql
@@ -0,0 +1,38 @@
+-- Drop database if it already exists
+DROP DATABASE IF EXISTS moffat_lodge;
+
+-- Create a fresh database
+CREATE DATABASE moffat_lodge;
+USE moffat_lodge;
+
+-- USER table
+CREATE TABLE user (
+  user_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  email VARCHAR(254) NOT NULL UNIQUE,
+  password_hash CHAR(60) NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  telephone VARCHAR(25) NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+-- ROOM TYPE table
+CREATE TABLE room_type (
+  room_type_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(32) NOT NULL UNIQUE,
+  price_per_night DECIMAL(8,2) NOT NULL,
+  PRIMARY KEY (room_type_id)
+);
+
+-- RESERVATION table
+CREATE TABLE reservation (
+  reservation_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id BIGINT UNSIGNED NOT NULL,
+  room_type_id TINYINT UNSIGNED NOT NULL,
+  guests TINYINT UNSIGNED NOT NULL,
+  check_in DATE NOT NULL,
+  check_out DATE NOT NULL,
+  PRIMARY KEY (reservation_id),
+  FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (room_type_id) REFERENCES room_type(room_type_id)
+);
