37047309a830169285fd348d1536aa8f91a0b32c
nima memarzadeh <nmemarcoding@gmail.com>
2025-09-02 08:18:16 -0700

    chore: add Spring Boot project setup with MySQL, JPA, Security, JWT, and base config

---
+++ backend/moffat-bay/src/main/java/com/group2/moffat_bay/MoffatBayApplication.java
@@ -0,0 +1,13 @@
+package com.group2.moffat_bay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoffatBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoffatBayApplication.class, args);
+	}
+
+}
